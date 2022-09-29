1c19e9ac7d7c1f04c3dd61bb92ba1d21b45df776
Evgira <bevgira@gmail.com>
2022-09-29 12:27:07 +0200

    created register form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import { redirect } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
+import Register from "./Register";
 
 export default function Navbar() {
   const [user, setUser] = useState({});
   const [users, setUsers] = useState([]);
 
+  function RegisterFormShow() {
+   
+  }
+
   return (
     <div>
-      <h3>Something</h3>
+      <button onClick={RegisterFormShow}>Register</button>
       <FaRegUserCircle />
     </div>
   );
